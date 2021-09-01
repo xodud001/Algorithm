bfd0550dc28436c1dd236f540eb425bb289b6536
김태영 <51165061+xodud001@users.noreply.github.com>
2021-09-01 23:33:33 +0900

    알고리즘 문제 풀이 수정
    볼링공 고르기 책 정답 알고리즘 추가

--- src/greedy/ChooseBall.java
+++ src/greedy/ChooseBall.java
@@ -3,13 +3,33 @@ package greedy;
 public class ChooseBall {
     public static void main(String[] args) {
         int[] balls = {1, 5, 4, 3, 2, 4, 5, 2};
-        int count = 0;
 
+        System.out.println("My Solution : " + mySolution(balls));
+        System.out.println("Book Solution : " + bookSolution(balls));
+    }
+
+    public static int mySolution(int[] balls){
+        int count = 0;
         for(int i = 0 ; i < balls.length - 1 ; i++){
             for(int j = i+1 ; j < balls.length ; j++){
                 if(balls[i] != balls[j]) count++;
             }
         }
-        System.out.println(count);
+        return count;
+    }
+
+    public static int bookSolution(int[] balls){
+        int n = balls.length;
+        int count = 0;
+        int[] ballsWeight = new int[11];
+
+        for (int ball : balls)
+            ballsWeight[ball] += 1;
+
+        for (int weight : ballsWeight) {
+            n -= weight;
+            count += weight * n;
+        }
+        return count;
     }
 }
